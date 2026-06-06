deddb97a3c0c6ecfc6b62fa4d3de7512c400466f
barryroodt <barry@jumptag.co.za>
2026-06-06 08:52:58 +0200

    feat(core): public barrel export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,48 @@
-export const SCHEMA_VERSION = "1.0";
+export {
+  SCHEMA_VERSION,
+  VALID_OUTPUT_FORMATS,
+  VALID_PROTOCOL_VERSIONS,
+  getToolVersion,
+  setToolVersion,
+} from "./version.js";
+export {
+  AgentError,
+  type AgentErrorInit,
+  ExitCode,
+  type ExitCodeValue,
+  type ExitFn,
+  newToolError,
+  newUserError,
+} from "./errors.js";
+export { Logger, stripAnsi } from "./logger.js";
+export { type ModeInput, type OutputFormat, type ResolvedMode, resolveMode } from "./mode.js";
+export { type ProgressEvent, Writer, type WriterInit } from "./writer.js";
+export type {
+  ArgumentMetadata,
+  Example,
+  FlagAnnotation,
+  Metadata,
+  ReturnSchema,
+} from "./metadata.js";
+export {
+  applyFlagAnnotation,
+  type Capabilities,
+  capabilitiesToWire,
+  type CommandSchema,
+  commandSchemaToWire,
+  type DescribeCommandSchema,
+  describeCommandToWire,
+  type DescribeOutput,
+  describeOutputToWire,
+  defaultCapabilities,
+  emitSchema,
+  type FlagSchema,
+  type ProfilesInfo,
+  type SafetyBlock,
+  safetyFromMetadata,
+  type SubcommandSchema,
+} from "./schema.js";
+export { type Profile, ProfileStore, profilePath } from "./profiles.js";
+export { checkConventions } from "./conventions.js";
+export { formatAgentsMd } from "./agentsmd.js";
+export { type ConformanceOptions, type ConformanceResult, runConformance } from "./conformance.js";
